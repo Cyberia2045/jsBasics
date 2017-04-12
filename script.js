66784a6cf33f443043b3ac93f74534b736604e90
Cyberia 2045 <sagansociety86@gmail.com>
2017-04-12 15:10:59 -0400

    multidimensional arrays

--- script.js
+++ script.js
@@ -14,16 +14,50 @@
 // }; // Ends the hungry function
 
 
-function games(status) {
+// function games(status) {
 
-if (status === "monopoly") {
-	alert("Let's Play Monopoly")
-}
-else if (status === "chess") {
-	alert("Let's Play Chess")
-}
-else { 
-	alert("You Must Make Decisions!")
+// var status = document.getElementById("status").value;
+
+// console.log("status")
+
+// if (status === "monopoly") {
+// 	alert("Let's Play Monopoly")
+// }
+// else if (status === "chess") {
+// 	alert("Let's Play Chess")
+// }
+// else { 
+// 	alert("You Must Make Decisions!")
+// };
+
+// }; //closes the games function
+
+// var message = "Hello";
+
+// var friends = ["John", "Rocco", "Ty", "Joe", "Eric"];
+
+// 	for(var i=0; i < friends.length; i++) {
+// 		console.log(friends[i] + " is my friend");
+// };
+
+var kellogs = ["Corn Flakes", "Frosted Flakes", "Apple Jacks"];
+
+var post = ["Raisin Bran", "Cookie Crisp", "Honey Smacks"];
+
+var cereal = [kellogs, post];
+
+for(var i=0; i < cereal.length; i++) {
+	for(var j=0; j < cereal[i].length; i++) {
+		console.log(cereal[i][j])
+	};
 };
 
-}; //closes the games function
+
+
+
+
+
+
+
+
+
