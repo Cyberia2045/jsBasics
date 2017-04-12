a741a64628aa9c9884e5811fbccc62085d9a89db
Cyberia 2045 <sagansociety86@gmail.com>
2017-04-12 11:42:00 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// function hungry() {
+// 	var hungry = "Yes";
+// 	var bored = "Yes";
+
+// 	if (hungry === "Yes") {
+// 		alert("Go Eat");
+// 	}
+// 	else if (bored === "Yes") {
+// 		alert("Take a Look in the Fridge");
+// 	}
+// 	else {
+// 		alert("Do Nothing")
+// 	}
+// }; // Ends the hungry function
